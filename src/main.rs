ea550eccc41269e107f4e6c314f379e2f1455f38
Morteza <mshojaei179@gmail.com>
2023-07-09 15:40:14 +0330

    server strcture initlization

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+fn main() {
+    let server = Server::new("127.0.0.1:8080".to_string());
+    server.run()
+}
+
+struct Server {
+    addr: String,
+}
+
+impl Server {
+    fn new(addr: String) -> Self {
+        Server { addr }
+    }
+    fn run(self) {
+        println!("Runing on {}", self.addr);
+    }
+}
