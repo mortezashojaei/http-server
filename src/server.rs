9602662f716b1c9735a709ebe172cb6105f5a3b9
Morteza <mshojaei179@gmail.com>
2023-07-15 17:10:15 +0330

    implement http requests log, with TCP listener

--- src/server.rs
+++ src/server.rs
@@ -1,3 +1,6 @@
+use std::io::Read;
+use std::net::TcpListener;
+
 pub struct Server {
     addr: String,
 }
@@ -8,5 +11,24 @@ impl Server {
     }
     pub fn run(self) {
         println!("Runing on {}", self.addr);
+        let listener = TcpListener::bind(&self.addr).unwrap();
+        loop {
+            match listener.accept() {
+                Ok((mut stream, _)) => {
+                    let mut buffer = [0; 1024];
+                    match stream.read(&mut buffer) {
+                        Ok(_) => {
+                            print!("Received a request: {}", String::from_utf8_lossy(&buffer))
+                        }
+                        Err(e) => {
+                            println!("Error Happend: {}", e);
+                        }
+                    }
+                }
+                Err(e) => {
+                    println!("Error Happend: {}", e);
+                }
+            }
+        }
     }
 }
