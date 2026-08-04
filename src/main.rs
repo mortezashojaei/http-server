e5372b0b080c739883901a93c98fd871217d2cb4
Morteza Shojaei <mshojaei179@gmail.com>
2026-08-05 00:40:41 +0100

    Drop AppHandler and route /api from WebsiteHandler.

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,14 @@
 mod api_handler;
-mod app_handler;
 mod http;
 mod server;
 mod website_handler;
 
 use api_handler::ApiHandler;
-use app_handler::AppHandler;
 use server::Server;
+use website_handler::WebsiteHandler;
 
 fn main() {
     let server = Server::new("127.0.0.1:8080".to_string());
     let api = ApiHandler::new("data/items.txt");
-    server.run(AppHandler::new(api));
+    server.run(WebsiteHandler::new(api));
 }
--- src/website_handler.rs
+++ src/website_handler.rs
@@ -1,10 +1,23 @@
+use super::api_handler::ApiHandler;
 use super::http::{Method, Request, Response, StatusCode};
 use super::server::Handler;
 
-pub struct WebsiteHandler;
+pub struct WebsiteHandler {
+    api: ApiHandler,
+}
+
+impl WebsiteHandler {
+    pub fn new(api: ApiHandler) -> Self {
+        WebsiteHandler { api }
+    }
+}
 
 impl Handler for WebsiteHandler {
     fn handle_request(&mut self, request: &Request) -> Response {
+        if request.path().starts_with("/api") {
+            return self.api.handle_request(request);
+        }
+
         match request.method() {
             Method::GET => match request.path() {
                 "/" => Response::new(StatusCode::Ok, Some("Welcome".to_string())),
